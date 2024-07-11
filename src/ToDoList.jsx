c278022e1b81474e04fd8c183177f6ff8b2a50fb
katemba fahad <katsfahad008@gmail.com>
2024-07-11 11:46:55 +0300

    Add files for to do list app

---
+++ src/ToDoList.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const ToDoList = () => {
+
+    const [tasks, setTasks] = useState([])
+
+    const [newTask, setNewTask] = useState('')
+
+    const handleInputTask = (e) =>{
+        setNewTask(e.target.value)
+    }
+
+    const addTask = () =>{
+        if(newTask){
+            setTasks(t=> [...t, newTask])
+            setNewTask("")
+        }
+        
+    }
+    const deleteTask = (index) =>{
+        setTasks(t=> t.filter((_, i)=> i !== index))
+    }
+    const moveTaskUp = (index) => {
+        if (index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]]
+        setTasks(updatedTasks)}
+    
+      };
+      const moveTaskDown = (index) => {
+        if (index < tasks.length - 1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]]
+        setTasks(updatedTasks)}
+    
+      };
+
+    return ( 
+        <div className="to-do-list">
+            <div>
+                <input type="text" onChange={handleInputTask} placeholder="Enter Task"/>
+                <button onClick={addTask} className="add-btn">Add Task</button>
+            </div>
+            <ol>
+                {
+                    tasks.map((item, index)=>{
+                        return <li key={index}><span className="text">{item}</span><button onClick={()=>deleteTask(index)} className="delete-btn">Delete Task</button><button onClick={()=>moveTaskUp(index)} className="move-btn">⬆️</button><button onClick={()=>moveTaskDown(index)} className="move-btn">⬇️</button></li>
+                    })
+
+                }
+            </ol>
+        </div>
+     );
+}
+ 
+export default ToDoList;
